ca16dacaa1bf8e4495c36f7a37de8a31635bc305
hicey <hiceycook@gmail.com>
2021-07-26 18:56:26 -0400

    errors handled

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,11 +9,21 @@ var getUserRepos = function (user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function (response) {
-        response.json().then(function (data) {
-            displayRepos(data, user);
+    fetch(apiUrl)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert('Error: GitHub User Not Found');
+            }
+        })
+        .catch(function (error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to GitHub");
         });
-    });
 };
 
 
@@ -35,6 +45,12 @@ let displayRepos = function (repos, searchTerm) {
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
+
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
 
     // loop over repos
     for (var i = 0; i < repos.length; i++) {
