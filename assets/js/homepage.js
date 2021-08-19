ad639677eaa2b9344384b91f3c83f73bdc4a32c1
hicey <hiceycook@gmail.com>
2021-08-19 13:41:11 -0400

    search by language added

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,9 @@ let userFormEl = document.querySelector("#user-form");
 let nameInputEl = document.querySelector("#username");
 let repoContainerEl = document.querySelector("#repos-container");
 let repoSearchTerm = document.querySelector("#repo-search-term");
+let languageButtonsEl = document.querySelector("#language-buttons");
+
+
 
 
 let getUserRepos = function (user) {
@@ -89,9 +92,29 @@ let displayRepos = function (repos, searchTerm) {
     }
 };
 
+let getFeaturedRepos = function (language) {
+    let apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    });
+};
 
+let buttonClickHandler = function (event) {
+    let language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    }
+};
 
 
 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
